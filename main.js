961c50aacad97b992bab44355c7105aae48e717d
Danny Ruda <danny.ruda1997@gmail.com>
2022-10-16 21:22:35 +0200

    Adds functionality to add a book card when submitting form
    Clicking on the submit button of the form for the book data will create
    a new book card on the page with the data currently filled in the
    input fields of the form.

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+let bookArray = [];
+
+function Book(title, author, pages, read, number) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this._number = number;
+}
+
+function changeReadStatus () {
+    this.read = this.read ? false : true;
+}
+
+function getFormData() {
+    return new FormData(document.querySelector('form'));
+}
+
+function addBooktoHtml(book) {
+    let htmlBook = document.createElement('div');
+    htmlBook.classList.add('book');
+    htmlBook.setAttribute('data-BookIndexNumber',book.bookIndexNumber);
+    let bookTitle = document.createElement('h1');
+    let bookAuthor = document.createElement('h2');
+    let bookPages = document.createElement('h2');
+    let bookReadStatus = document.createElement('button');
+    let bookRemove = document.createElement('button');
+    bookTitle.innerText = book.title;
+    bookAuthor.innerText = book.author;
+    bookPages.innerText = book.pages;
+    bookReadStatus.innerText = book.read ? 'read' : 'not read';
+    bookRemove.innerText = 'Remove';
+    htmlBook.append(bookTitle,bookAuthor,bookPages,bookReadStatus,bookRemove)
+    document.querySelector('.books').append(htmlBook);
+}
+
+let bookIndexNumber = 1;
+function addBook(event) {
+    event.preventDefault();
+    console.log('enters addBook')
+    let formData = getFormData();
+    console.log('enters addBook')
+    let book = new Book(formData.title, formData.author, formData.pages, formData.read, bookIndexNumber);
+    bookIndexNumber++;
+    bookArray.push(book);
+    addBooktoHtml(book);
+}
+
+Book.prototype.changeReadStatus = changeReadStatus;
+
 let buttonAddBook = document.querySelector('.addBook button');
 let newBookForm = document.querySelector('.bookForm');
 function buttonClicked() {
@@ -6,3 +56,6 @@ function buttonClicked() {
 }
 
 buttonAddBook.addEventListener('click', buttonClicked);
+
+let submitButton = document.querySelector('.submit');
+submitButton.addEventListener('click', addBook);
