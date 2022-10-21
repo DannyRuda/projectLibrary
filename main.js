540ebaa569493ae6197600e6d3ee8bb0cfd0853a
Danny Ruda <danny.ruda1997@gmail.com>
2022-10-21 16:32:03 +0200

    Adds comments and changing read status posibility
    Adds comments to code increasing the readability and adds
    functionality that changes the read status on the book card and
    in the book object when the read status button is pressed.

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+// bookArray stores all book objects that will be created  
 let bookArray = [ {title: 'Harry Potter and the Philosophers Stone',
                    author: 'J.K. Rowling',
                    pages: '336',
                    read: false,
-                   _number: 0},
+                   _number: 0,
+                   changeReadPropertyValue: changeReadPropertyValue},
 
                   {title: 'A Tale of Two Citys',
                    author: 'Charles Dickens',
                    pages: '544',
                    read: true,
-                   _number: 1
+                   _number: 1,
+                   changeReadPropertyValue: changeReadPropertyValue
                   }];
 
+
+
+// if bookArray can later be saved to an account, this function will
+// display the book objects from bookArray
 function displaySavedBooks(bookArray) {
     for (let book of bookArray) {
         addBooktoHtml(book);
@@ -19,6 +26,7 @@ function displaySavedBooks(bookArray) {
 
 displaySavedBooks(bookArray);
 
+// constructor for book objects, _number will be used for identification
 function Book(title, author, pages, read, number) {
     this.title = title;
     this.author = author;
@@ -27,10 +35,9 @@ function Book(title, author, pages, read, number) {
     this._number = number;
 }
 
-function changeReadStatus () {
-    this.read = this.read ? false : true;
-}
-
+// stores the values of the form elements in an array and returns that array
+// so the function can be called to use the form data when creating a new 
+//book object 
 function getFormData() {
     let formData = new FormData(document.querySelector('form'));
     let formDataArray = [];
@@ -40,11 +47,11 @@ function getFormData() {
         }
     }
     formDataArray.push(document.querySelector('#read').checked)
-    console.log(formDataArray);
     return formDataArray;
 }
 
-
+// creates html elements according to the current values of the form and adds 
+// these elements to the DOM to display the book Data in a card
 function addBooktoHtml(book) {
     let htmlBook = document.createElement('div');
     htmlBook.classList.add('book');
@@ -58,6 +65,7 @@ function addBooktoHtml(book) {
     bookAuthor.innerText = book.author;
     bookPages.innerText = book.pages;
     bookReadStatus.innerText = book.read ? 'read' : 'not read';
+    bookReadStatus.addEventListener('click',changeReadStatus);
     bookRemove.innerText = 'Remove';
     bookRemove.classList.add('lastButton', 'remove');
     bookRemove.addEventListener('click', removeBook);
@@ -68,11 +76,13 @@ function addBooktoHtml(book) {
 //bookIndexNumber will count like array indexes, starting from 0
 let bookIndexNumber = bookArray.length;
 
+// prevents the form from reloading the page when submitting, creates new book 
+// object with the getFormData function, adds it to the book array, creates and 
+// modifies html elements to display book data and resets form values
 function addBook(event) {
     event.preventDefault();
     let formDataArray = getFormData();
     let book = new Book(...formDataArray, bookIndexNumber);
-    console.log(book);
     bookIndexNumber++;
     bookArray.push(book);
     addBooktoHtml(book);
@@ -84,6 +94,7 @@ function resetFormValues() {
     document.querySelector('form').reset();
 }
 
+// removes book from bookArray and book card from the website
 function removeBook(event) {
     let bookIdent = event.target.parentElement.dataset.bookIndexNumber;
     event.target.parentElement.remove();
@@ -95,22 +106,40 @@ function removeBook(event) {
     bookArray.splice(bookArray.indexOf('bookIdent'))
 }
 
-Book.prototype.changeReadStatus = changeReadStatus;
-
+//creates variables representing the addBook button and the form
 let buttonAddBook = document.querySelector('.addBook button');
 let newBookForm = document.querySelector('.bookForm');
+
+// shows the form 
 function buttonClicked() {
-    console.log(newBookForm.classList);
     newBookForm.classList.remove('hide');
 }
 
+
 buttonAddBook.addEventListener('click', buttonClicked);
 
+// creates variable referencing to the submit button of the form
 let submitButton = document.querySelector('.submit');
+// clicking the submit button calls the addBook function
 submitButton.addEventListener('click', addBook);
 
 let removeButtonArray = [...document.getElementsByClassName('remove')];
 
 for (let removeButton of removeButtonArray) {
     removeButton.addEventListener('click', removeBook);
+}
+
+// changes read property value from calling book oject to false or true 
+function changeReadPropertyValue () {
+    this.read = this.read ? false : true;
+}
+
+// adds function as a method to prototype of objects created with Book constructor
+Book.prototype.changeReadPropertyValue = changeReadPropertyValue;
+
+// changes read status in book object and in the button on the book card on the website
+function changeReadStatus(event) {
+    let bookIdent = event.target.parentElement.dataset.bookIndexNumber;
+    event.target.innerText = bookArray[bookIdent].read ? 'not read' : 'read';
+    bookArray[bookIdent].changeReadPropertyValue(); 
 }
